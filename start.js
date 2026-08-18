e4b8664ac8c38f57c35dd8a2dcd2563934c4e6ff
OzarkRoosts <theozarkroost@gmail.com>
2026-08-18 16:24:20 -0500

    feat: arm funding opportunity agent at startup

--- start.js
+++ start.js
@@ -4,6 +4,8 @@ async function start() {
   const pool = require('./db/index');
   const { runAllMigrations } = require('./migrate-runner');
   await runAllMigrations(pool);
+  const fundingAgent = require('./lib/funding-opportunity-agent');
+  fundingAgent.start();
   require('./server');
 }
 
